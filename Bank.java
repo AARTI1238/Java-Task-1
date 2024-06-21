6600359f61b94337d649dcce85cf637cf676c340
AARTI1238 <goswamiaarti0000@gmail.com>
2024-06-21 22:20:50 +0530

    Bank1.java

--- Bank.java
+++ Bank.java
@@ -1,38 +1,34 @@
-
-Package bank_account;
-
-Public class bank_account
+public class bank_account
 {
-Private double balance;
-Public bank_account(double open_balance
+private double balance;
+public bank_account(double open_balance)
 {
 balance = open_balance;
 }
-Public void display()
+public void display()
 {
 System.out.println("your account is:+" +balance);
 }
-Public void deposit(double amount)
+public void deposit(double amount)
 {
-Balance = balance + amount;
+balance = balance + amount;
 }
-Public void withdraw(double  amount)
-{
-balance = balance - amount;
-}
-Public void transfer(double amount)
+public void withdraw(double  amount)
 {
 balance = balance - amount;
 }
-Public static void main(String args[])
+public void transfer(double amount)
+{
+balance = balance - amount;
+}
+public static void main(String args[])
 bank_account tran1 = new bank_account(100000);
 System.out.println("balance before:");
 tran1.display();
 System.out.println("balance after transaction:");
-tran1.deposit(50000);
-tran2.withdraw(20000);
-tran3.transfer(12000);
+tran1.deposit(50000.0);
+tran1.withdraw(20000.0);
+tran1.transfer(12000.0);
 System.out.println("show balance after transaction:");
 tran1.display();
-}
 }
